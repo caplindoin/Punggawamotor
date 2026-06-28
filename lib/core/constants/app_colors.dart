d74c3de445d2a70a3e2cc40f61f423317647c6e8
caplindoin <wahyupunggawa@gmail.com>
2026-06-28 21:37:22 +0700

    Add app color constants (Biru tua, putih, abu-abu)

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary Colors
+  static const Color primaryDark = Color(0xFF1a365d);
+  static const Color primaryLight = Color(0xFF2d5a8c);
+  
+  // Secondary Colors
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color grey = Color(0xFF9CA3AF);
+  static const Color greyLight = Color(0xFFF3F4F6);
+  static const Color greyDark = Color(0xFF6B7280);
+  
+  // Status Colors
+  static const Color success = Color(0xFF10B981);
+  static const Color warning = Color(0xFFF59E0B);
+  static const Color error = Color(0xFFEF4444);
+  static const Color info = Color(0xFF3B82F6);
+  
+  // Background
+  static const Color background = Color(0xFFFAFAFA);
+  static const Color surfaceLight = Color(0xFFFEFEFE);
+  
+  // Text Colors
+  static const Color textPrimary = Color(0xFF1F2937);
+  static const Color textSecondary = Color(0xFF6B7280);
+}
