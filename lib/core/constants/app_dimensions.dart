c3a34e7d0bd3d7d5780ac90b2e5e1d8ab1f3ed82
caplindoin <wahyupunggawa@gmail.com>
2026-06-28 21:37:32 +0700

    Add app dimensions constants

---
+++ lib/core/constants/app_dimensions.dart
@@ -0,0 +1,37 @@
+class AppDimensions {
+  // Padding & Margin
+  static const double paddingXs = 4.0;
+  static const double paddingSm = 8.0;
+  static const double paddingMd = 16.0;
+  static const double paddingLg = 24.0;
+  static const double paddingXl = 32.0;
+  
+  // Border Radius
+  static const double radiusSm = 4.0;
+  static const double radiusMd = 8.0;
+  static const double radiusLg = 12.0;
+  static const double radiusXl = 16.0;
+  
+  // Icon Sizes
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+  static const double iconXl = 48.0;
+  
+  // Font Sizes
+  static const double fontSm = 12.0;
+  static const double fontMd = 14.0;
+  static const double fontLg = 16.0;
+  static const double fontXl = 18.0;
+  static const double fontXxl = 24.0;
+  static const double font2xl = 28.0;
+  static const double font3xl = 32.0;
+  
+  // Heights
+  static const double buttonHeight = 48.0;
+  static const double appBarHeight = 56.0;
+  static const double cardHeight = 120.0;
+  
+  // Widths
+  static const double buttonWidth = 120.0;
+}
